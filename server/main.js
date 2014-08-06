2bc6b8d4443dd44b1888263aa58a6f643376ea3b
theepicsnail <theepicsnail@gmail.com>
2014-08-06 01:49:06 -0400

    Moved to gulp. With live reload !

--- server/main.js
+++ server/main.js
@@ -1,7 +1,14 @@
-root = __dirname + "/.."
+root = __dirname + "/..";
 var express = require('express');
+var livereload = require('connect-livereload');
+var body = require('body-parser');
+
 app = express();
+app.use(body());
+app.use(livereload())
 app.use(express.static(root+ '/app'));
 app.use('/bower_components',  express.static(root + '/bower_components'));
 app.get('/', function (req,res) { return res.render('index.html'); });
+
+console.log("theepicsnail.net:3000");
 app.listen(3000);

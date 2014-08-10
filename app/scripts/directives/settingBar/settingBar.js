559ad15ab1c081b5e27fc4ed45e60450e27c8dfc
theepicsnail <theepicsnail@gmail.com>
2014-08-10 16:23:56 -0400

    Refactor

--- app/scripts/directives/settingBar/settingBar.js
+++ app/scripts/directives/settingBar/settingBar.js
@@ -6,25 +6,29 @@
  * @description
  * # settingBar
  */
+
+var scripts = document.getElementsByTagName('script');
+var currentScriptPath = scripts[scripts.length-1].src;
+
 angular.module('angularbootApp')
   .directive('settingBar', function () {
     return {
-      templateUrl: 'views/settingBar.html',
+      templateUrl: currentScriptPath.substring(0, currentScriptPath.lastIndexOf('/') + 1) + 'settingBar.html',
       scope: {
         setting:'=settingBar'
       },
       controller: function($scope) {
-        $scope.updatePercentage= 
+        $scope.updatePercentage=
           function(setting, value) {
-          var newVal = setting.value + value
+          var newVal = setting.value + value;
           if (newVal < 0) {
             newVal = 0;
           }
           else if(newVal > 100) {
-            newVal = 100
+            newVal = 100;
           }
-          setting.value = newVal
-        }
+          setting.value = newVal;
+        };
       }
     };
   });

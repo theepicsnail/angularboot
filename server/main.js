33613ab1bbfb995f041a1e7b9cfdd71a65fb86d7
theepicsnail <theepicsnail@gmail.com>
2014-09-03 11:34:46 -0700

    Added useful sockjs communication

--- server/main.js
+++ server/main.js
@@ -4,12 +4,24 @@ module.exports = function(server) {
   var sjsserver = sockjs.createServer(
     { sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js" }
   );
-  sjsserver.on('connection', function(conn) { 
-    console.log('connect'); 
+
+  var clients = [];
+  var last_data = "";
+  sjsserver.on('connection', function(conn) {
+    clients.push(conn);
+    if (last_data) {
+      conn.write(last_data); // Update the client
+    }
+    conn.on('close', function () {
+      clients.pop(clients.indexOf(conn));
+    });
     conn.on('data', function(data) {
-      console.log('data', data);
-      conn.write(data);
-      conn.write(0);
+      last_data = data;
+      for(var id in clients) {
+        if (clients[id] !== conn) {
+          clients[id].write(data);
+        }
+      }
     });
   });
 
